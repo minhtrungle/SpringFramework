8874240210a4c6d2a73f73c4045f5ae383f61d04
minhtrungle <minhtrungle21599@gmail.com>
2023-04-21 00:22:26 +0700

    Update Config

--- src/main/java/com/programming/springframework/spring/AppConfig.java
+++ src/main/java/com/programming/springframework/spring/AppConfig.java
@@ -1,4 +1,27 @@
 package com.programming.springframework.spring;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = "com.programming.springframework.spring") // Quét cả package
 public class AppConfig {
+
+    // Sử dụng AppConfig để thay cho cấu hình theo Beans.xml
+    @Bean(name = "basicSpellChecker", initMethod = "init", destroyMethod = "destroy")
+    public BasicSpellChecker createBasicSpellChecker() {
+        return new BasicSpellChecker();
+    }
+
+    @Bean(name = "advancedSpellChecker")
+    public AdvancedSpellChecker createAdvancedSpellChecker() {
+        return new AdvancedSpellChecker();
+    }
+
+    // Gọi 1 trong 2 bean ở trên nó sẽ được truyền vào EmailApplication
+    @Bean(name = "emailClient")
+    public EmailClient createEmailClient() {
+//        EmailClient emailClient = new EmailClient();
+//        emailClient.setSpellChecker(createBasicSpellChecker());
+        return new EmailClient(createBasicSpellChecker());
+    }
 }
