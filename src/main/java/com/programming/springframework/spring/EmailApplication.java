cafe7b56a2a7672f8ea5a86ba57f73754c1b9967
minhtrungle <minhtrungle21599@gmail.com>
2023-04-21 00:22:03 +0700

    Update EmailClient

--- src/main/java/com/programming/springframework/spring/EmailApplication.java
+++ src/main/java/com/programming/springframework/spring/EmailApplication.java
@@ -1,4 +1,18 @@
 package com.programming.springframework.spring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class EmailApplication {
+    public static void main(String[] args) {
+
+        // Cấu hình bằng xml
+        //ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+        // Cấu hình bằng java
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        EmailClient emailClient = applicationContext.getBean("emailClient", EmailClient.class);
+        emailClient.sendEmail("Text 1");
+        emailClient.sendEmail("Text 2");
+    }
 }
--- src/main/java/com/programming/springframework/spring/EmailClient.java
+++ src/main/java/com/programming/springframework/spring/EmailClient.java
@@ -11,6 +11,27 @@ public class EmailClient {
     // Tiêm object SpellChecker vào EmailClient
     private SpellChecker spellChecker;
 
+    //Qualifier thì bỏ đoạn dưới dùng contructor và setter
+//    public EmailClient() {
+//    }
+//
+//    @Autowired
+//    EmailClient(SpellChecker spellChecker) {
+//        this.spellChecker = spellChecker;
+//    }
+//
+//    public SpellChecker getSpellChecker() {
+//        return spellChecker;
+//    }
+//
+//    @Autowired
+//    public void setSpellChecker(SpellChecker spellChecker) {
+//        this.spellChecker = spellChecker;
+//    }
+
+    public EmailClient(BasicSpellChecker basicSpellChecker) {
+    }
+
     public void sendEmail(String email) {
         spellChecker.checkSpelling(email);
     }
