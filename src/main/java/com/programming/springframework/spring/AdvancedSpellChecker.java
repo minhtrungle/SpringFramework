ed3487d45638aef798b2337077c700a660f3e5ff
minhtrungle <minhtrungle21599@gmail.com>
2023-04-21 00:21:22 +0700

    Update AdvancedSpellChecker

--- src/main/java/com/programming/springframework/spring/AdvancedSpellChecker.java
+++ src/main/java/com/programming/springframework/spring/AdvancedSpellChecker.java
@@ -1,4 +1,41 @@
 package com.programming.springframework.spring;
 
-public class AdvancedSpellChecker {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource(value = "classpath:/application.properties")
+public class AdvancedSpellChecker implements SpellChecker, InitializingBean, DisposableBean {
+
+
+    @Value("${app.database.uri}")
+    private String databaseUri;
+
+    @Override
+    public void checkSpelling(String emailMessage) {
+        if (emailMessage != null) {
+            // Determining the Language of the email Message
+            // Check grammatical errors in the email Message
+            // Check spellings in the email Message
+            System.out.println("Checking Spelling using Advanced Spell Checker..");
+            System.out.println("Spell Checking Completed!!");
+
+            System.out.println("DB URI: " + databaseUri);
+        } else {
+            throw new RuntimeException("An exception occurred while checking Spelling");
+        }
+    }
+
+    public void destroy() {
+        System.out.println("Destroy");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Setting Properties after Bean is initialized");
+    }
 }
