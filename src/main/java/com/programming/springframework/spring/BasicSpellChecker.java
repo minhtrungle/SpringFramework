666a1caba3107b299c5556ae16b3b33789abf193
minhtrungle <minhtrungle21599@gmail.com>
2023-04-21 00:21:43 +0700

    Update BasicSpellChecker

--- src/main/java/com/programming/springframework/spring/BasicSpellChecker.java
+++ src/main/java/com/programming/springframework/spring/BasicSpellChecker.java
@@ -1,4 +1,26 @@
 package com.programming.springframework.spring;
 
-public class BasicSpellChecker {
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+//@Component
+//@Primary
+public class BasicSpellChecker implements SpellChecker{
+    // Trước khi khởi tạo phương thức
+    public void init() {
+        System.out.println("Before create method");
+    }
+
+    @Override
+    public void checkSpelling(String emailMessage) {
+        if (emailMessage != null) {
+            System.out.println("Spell Checking Completed!!");
+        } else {
+            throw new RuntimeException("Error");
+        }
+    }
+
+    public void destroy() {
+        System.out.println("Destroy method");
+    }
 }
