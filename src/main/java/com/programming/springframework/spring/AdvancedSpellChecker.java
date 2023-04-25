f7e89696cfc60964f74ef5d29314eec5872961ff
minhtrungle <minhtrungle21599@gmail.com>
2023-04-25 21:07:18 +0700

    Update AdvancedSpellChecker

--- src/main/java/com/programming/springframework/spring/AdvancedSpellChecker.java
+++ src/main/java/com/programming/springframework/spring/AdvancedSpellChecker.java
@@ -20,9 +20,6 @@ public class AdvancedSpellChecker implements SpellChecker, InitializingBean, Dis
     @Override
     public void checkSpelling(String emailMessage) {
         if (emailMessage != null) {
-            // Determining the Language of the email Message
-            // Check grammatical errors in the email Message
-            // Check spellings in the email Message
             System.out.println("Checking Spelling using Advanced Spell Checker..");
             System.out.println("Spell Checking Completed!!");
 
